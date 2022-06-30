c936d5308d0190344da44a722af61a6859c8929a
Cheeseman5 <dhuber04@gmail.com>
2022-06-29 20:54:14 -0500

    buffed pdf page card

--- src/PagePane.js
+++ src/PagePane.js
@@ -3,9 +3,19 @@ import './PagePane.css';
 import Pdf from './Pdf';
 
 export default function PagePane() {
+    //let pages;
     const PopulatePages = () => {
+        const name = 'test-doc-name.pdf';
         const Len = 100;
-        return Array(Len).fill(<Pdf/>);
+        let pages = Array(0);
+        
+        //console.log(`iterating to ${Array(Len).length}`);
+        for(let i=1; i <= Array(Len).length; i++) {
+            pages.push(<Pdf id={i} page={i} name={name}/>);
+            //console.log(`adding page ${i}`);
+        }
+        //console.log(pages);
+        return pages;
     }
     
     return (
--- src/Pdf.js
+++ src/Pdf.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Pdf.css';
 
-export default function Pdf() {
+export default function Pdf({ id, page, name='bob' }) {
     return (
-        <div className='Pdf'>
-            PDF.pdf
+        <div className='Pdf' key={id}>
+            <img className='card-image' src='default.png'/>
+            <div className='card-overlay'>
+                <h4 className='card-title'>{ name }</h4>
+                <p className='card-text'>page: {page}</p>
+            </div>
         </div>
     );
 }
