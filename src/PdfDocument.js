ac204e4dfd5436a3c5665bf333ef83a7f89b206d
Cheeseman5 <dhuber04@gmail.com>
2022-07-09 21:40:54 -0500

    tweeked doc naming

--- src/PdfDocument.js
+++ src/PdfDocument.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Pdf from './Pdf';
 
 export default function PdfDocument(props) {
-    
     let pages = props.pages || [];
+    let name = pages[0].name || props.name || '';
+
     const onClick = (event) => {
         props.onClick(event);
     }
     const PageName = () => {
-        return pages[0].name;
+        return name;
     }
     const PageImage = () => {
         return pages[0].image;
@@ -17,7 +18,7 @@ export default function PdfDocument(props) {
         return `${pages.length} pages`;
     }
     return (
-        <div className='pdf-document'>
+        <div className='pdf-document' id={props.id}>
             <Pdf id={pages[0].id} name={PageName()} image={PageImage()} pageText={PageText()} onClick={onClick} />
         </div>
     );
