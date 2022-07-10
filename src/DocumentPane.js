1c5cfaf29a513b41c52a9faaded45d33de9dd221
Cheeseman5 <dhuber04@gmail.com>
2022-07-09 21:41:38 -0500

    moved doc selection logic

--- src/DocumentPane.js
+++ src/DocumentPane.js
@@ -3,79 +3,15 @@ import './DocumentsPane.css';
 import Pdf from './Pdf';
 import PdfDocument from './PdfDocument';
 
-export default function DocumentsPane() {
-    let activeDocument;
-    let documents = [];
+export default function DocumentsPane(props) {
+    let documents = props.documents;
 
-    const toggleSelected = (li) => {
-        li.classList.toggle('selected');
-    }
-    const singleSelect = (pdfCard) => {
-        const selected = document.querySelectorAll('.page-pane .selected,.documents-pane .selected');
-        
-        for(let elem of selected) {
-            elem.classList.remove('selected');
-        }
-        pdfCard.classList.add('selected');
-    }
     const onClick = (event) => {
-        if(event.currentTarget.tagName !== 'LI') return;
-
-        if(event.ctrlKey || event.metaKey){
-            toggleSelected(event.currentTarget);
-        } else {
-            singleSelect(event.currentTarget);
-        }
-    }
-
-    const setActive = (doc) => {
-        activeDocument = doc;
-        console.log(`document '${doc.name}' now selected`);
-    };
-    const PopulateDocuments = () => {
-        const count = 20;
-        const pageCount = 50;
-        const name = 'testDocName.pdf';
-        let docs = Array(0);
-
-        [...Array(count).keys()].map(i => {
-            const pages = GeneratePages(pageCount, name);
-            docs.push(
-                <PdfDocument pages={pages} onClick={onClick}/>
-            )
-        });
-        documents = docs;
-        activeDocument = docs[0];
-        return docs;
-    }
-
-    const GeneratePages = (count, name) => {
-        let pages = [];
-        [...Array(count).keys()].map(i => {
-            const pageText = `page ${i}`;
-            pages.push(<Pdf id={i} name={name} pageText={pageText}/>)
-        });
-        return pages;
-    }
-
-    const PopulatePages = () => {
-        const name = 'test-doc-name.pdf';
-        const Len = 20;
-        let pages = Array(0);
-        
-        for(let i=1; i <= Array(Len).length; i++) {
-            pages.push(
-                <ul key={i}>
-                    <Pdf id={i} pageText={`${i} pages`} name={name}/>
-                </ul>
-            );
-        }
-        return pages;
+        props.onClick(event);
     }
 
     return (
         <div className='documents-pane'>
-            { PopulateDocuments() }
             { documents }
         </div>
     )
--- src/PdfToolbox.js
+++ src/PdfToolbox.js
@@ -3,14 +3,81 @@ import './PdfToolbox.css';
 import DocumentsPane from './DocumentPane';
 import Toolbar from './Toolbar';
 import PagePane from './PagePane';
+import Pdf from './Pdf';
+import PdfDocument from './PdfDocument';
 
 export default function PdfToolbox() {
+    let documents = [];
+    let activePages = [];
+
+    const toggleSelected = (li) => {
+        li.classList.toggle('selected');
+    }
+    const singleSelect = (pdfCard) => {
+        const selected = document.querySelectorAll('.page-pane .selected,.documents-pane .selected');
+        
+        for(let elem of selected) {
+            elem.classList.remove('selected');
+        }
+        pdfCard.classList.add('selected');
+    }
+    const onClick = (event) => {
+        if(event.currentTarget.tagName !== 'LI') return;
+
+        if(event.ctrlKey || event.metaKey){
+            toggleSelected(event.currentTarget);
+        } else {
+            singleSelect(event.currentTarget);
+        }
+        UpdatePages();
+    }
+
+    const PopulateDocuments = () => {
+        const count = 10;
+        const name = 'testDocName.pdf';
+        let docs = Array(0);
+
+        [...Array(count).keys()].map(() => {
+            const maxPages = Math.floor(Math.random() * (count - 5 + 1) + 5);
+            const pages = GeneratePages(maxPages, name);
+            docs.push(
+                <PdfDocument name={name} pages={pages} onClick={onClick}/>
+            )
+        });
+        documents = docs;
+    }
+
+    const GeneratePages = (count, name) => {
+        let pages = [];
+        [...Array(count).keys()].map(i => {
+            const pageText = `page ${i}`;
+            pages.push(<Pdf id={i} name={name} pageText={pageText}/>)
+        });
+        return pages;
+    }
+
+    const UpdatePages = () => {
+        const activeDocuments = document.querySelectorAll('.documents-pane .selected');
+        console.log(`activeDocuments: ${activeDocuments}`);
+        console.log(`activeDocuments?.length: ${activeDocuments?.length}`);
+        activePages = [];
+        activeDocuments.forEach((val, i, obj) => {
+        // for(const doc in activeDocuments) {
+            console.log(`doc: ${val.parentNode.pages}`);
+            // activePages.push(doc.pages);
+        });
+        console.log(`activeDocuments: ${activeDocuments}`);
+        console.log(`activePages: ${activePages?.length}`);
+        // return activePages;
+    }
+
     return (
         <div className='pdftoolbox'>
+            { PopulateDocuments() }
             <Toolbar />
             <div className='working-area'>
-                <DocumentsPane />
-                <PagePane />
+                <DocumentsPane documents={ documents } onClick={ onClick } />
+                <PagePane  activePages={ activePages } onClick={ onClick } />
             </div>
         </div>
     )
